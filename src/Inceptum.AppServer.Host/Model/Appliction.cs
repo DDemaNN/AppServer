694a2cdb5a1013f0baa2a7a6b4dbbee3a61276ba
Konstantin Nosov <knosov@inceptumsoft.com>
2014-09-07 18:23:32 +0400

    new project for experiments

---
+++ src/Inceptum.AppServer.Host/Model/Appliction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inceptum.AppServer.Host.Model
+{
+    public class Appliction
+    {
+        public string Name { get; set; }
+        public string PackageId { get; set; }
+        public string PackageVendor { get; set; }
+        public Version Version { get; set; }
+        public string DefaultConfiguration { get; set; }
+        public Instance[] Instances { get; set; }
+
+#region Defaults for instances
+        public bool AutoStart { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Environment { get; set; }
+        //TODO: use custom type
+        //public LoggerLevel LogLevel { get; set; }
+#endregion
+    }
+}
